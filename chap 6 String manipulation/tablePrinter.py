51afd15b2709e0f31cdaa5fb54684d0347f469e6
Mini CT <minicthomas921@gmail.com>
2019-09-02 08:46:04 -0700

    Completed chapter 6

--- chap 6 String manipulation/tablePrinter.py
+++ chap 6 String manipulation/tablePrinter.py
@@ -3,9 +3,35 @@
 Created on Thu Aug 29 20:27:40 2019
 
 @author: Minimol
-"""
-
+""" 
+ 
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
+
+
+
+def printTable(tableList):
+    colWidths = []
+    colWidths = [0] * len(tableList)
+   # print(colWidths)
+    # Finding the max width of strings in each coloumn
+    for i in range(0 , len(tableList[1])) :  
+        for j in range(0 , len(tableList)) :  
+            if len(tableList[j][i]) > colWidths[j] : 
+                colWidths[j] = len(tableList[j][i])
+        print()  
+   # print(colWidths)
+    
+    for i in range(0 , len(tableList[1])) :  
+        for j in range(0 , len(tableList)) :  
+            print('     ' + tableList[j][i] . rjust(colWidths[j]) , end=" ") 
+        print()   
+
+
+print()
+print('List OF List Display As a Grid' . center(50, '-'))
+printTable(tableData)
+
+    
